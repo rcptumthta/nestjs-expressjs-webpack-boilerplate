dc00c90197fed9cde7edd1b169b28961ea1a8e4c
rcptumthta <rcptum.thta@outlook.com>
2024-11-10 06:21:07 +0700

    build: set up configurations for builder application with Webpack

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const DotenvWebpackPlugin = require("dotenv-webpack");
+const ESLintWebpackPlugin = require("eslint-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+
+const path = require("node:path");
+const sourceMapSupport = require("source-map-support");
+
+sourceMapSupport.install({ environment: "node" });
+
+module.exports = function compile(option) {
+  return {
+    ...option,
+    output: {
+      filename: "main.js",
+      path: path.resolve(__dirname, "dist"),
+      libraryTarget: "commonjs2"
+    },
+    mode: process.env.NODE_ENV === "production" ? "production" : "development",
+    target: "node",
+    devtool: "source-map",
+    optimization: {
+      ...option.optimization,
+      minimizer: [
+        new TerserWebpackPlugin({
+          extractComments: false,
+          parallel: true,
+          terserOptions: {
+            ecma: 2022,
+            keep_classnames: true,
+            keep_fnames: true
+          }
+        })
+      ]
+    },
+    plugins: [
+      ...option.plugins,
+      new ESLintWebpackPlugin({
+        cache: false,
+        extensions: ["js", "ts"]
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: "environments",
+            to: "environments"
+          },
+          {
+            from: "resources",
+            to: "resources"
+          }
+        ]
+      }),
+      new DotenvWebpackPlugin({
+        systemvars: true,
+        silent: true
+      })
+    ]
+  };
+};

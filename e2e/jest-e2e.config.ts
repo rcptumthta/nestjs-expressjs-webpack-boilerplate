262b7d3e1018757e3cfc036f837389a1faf539c9
rcptumthta <rcptum.thta@outlook.com>
2024-11-10 03:49:04 +0700

    build(tsconfig): update configurations

---
+++ e2e/jest-e2e.config.ts
@@ -0,0 +1,19 @@
+import { resolve } from "node:path";
+import { JestConfigWithTsJest, pathsToModuleNameMapper } from "ts-jest";
+
+import { compilerOptions } from "../tsconfig.json";
+
+const configuration: JestConfigWithTsJest = {
+  testEnvironment: "node",
+  rootDir: ".",
+  testRegex: "\\.e2e-spec\\.ts$",
+  transform: {
+    "^.+\\.(t|j)s$": "ts-jest"
+  },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: resolve()
+  }),
+  moduleFileExtensions: ["js", "json", "ts"]
+};
+
+export default configuration;
